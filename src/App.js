26dfd6254c61a7063f6392161c9fb5a3f5bed180
Nicolas M. Pardo <nicolasm@nikodermus.media>
2018-05-21 17:22:20 -0500

    General clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import Cell from './Cell';
-
 import './App.css';
 
 class App extends Component {
@@ -13,31 +12,30 @@ class App extends Component {
   componentWillMount() {
     this.createGrid(50, {
       alive: false,
-      className: 'grid-elem',
     });
   }
 
-  createGrid = (num = 100, props = {}) => {
-
+  createGrid = (num = 100) => {
     // Fill grid with false rows and columns
     let { grid } = this.state;
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < num; i += 1) {
       grid.push([]);
-      for (let j = 0; j < num; j++) {
+      for (let j = 0; j < num; j += 1) {
         grid[i].push(false);
       }
     }
+
     // Add initial true cells
-    grid = this.setInitial(grid);
-    this.setState({ grid });
+    this.setInitial();
 
     // After a couple seconds, start evolving
     setTimeout(() => {
       this.checkGrid()
-    }, 10);
+    }, 1000);
   }
 
-  setInitial = (grid) => {
+  setInitial = () => {
+    let { grid } = this.state;
 
     grid[0][0] = true;
     grid[0][1] = true;
@@ -45,11 +43,11 @@ class App extends Component {
     grid[1][3] = true;
     grid[2][1] = true;
     grid[2][2] = true;
-    return grid;
+
+    this.setState({ grid });
   }
 
   renderGrid = () => {
-
     return this.state.grid.map((elem, i) => {
       return (
         <div className="row" key={`row-${i}`}>
@@ -66,21 +64,20 @@ class App extends Component {
   }
 
   checkGrid = () => {
-
     let { grid, generation } = this.state;
-    let alive_pop = 0;
     let future_grid = [...grid];
     grid.map((row, i) => {
-      row.map((elem, j) => {
-        let { alive_n, dead_n } = this.checkNeighbors(i, j);
+      row.map((col, j) => {
+        let { alive_n } = this.checkNeighbors(i, j);
 
-        // TODO: Wait for all info to be collected before making generation change
-        if ((grid[i][j] && alive_n === 2) || alive_n === 3) {
+        if (
+          alive_n === 3 ||
+          (grid[i][j] && alive_n === 2)
+        ) {
           future_grid[i][j] = true;
         } else {
           future_grid[i][j] = false;
         }
-
       });
     });
 
@@ -95,18 +92,17 @@ class App extends Component {
     if (generation < 50) {
       setTimeout(() => {
         this.checkGrid()
-      }, 10);
+      }, 1000);
     }
   }
 
   checkNeighbors = (i, j) => {
-
     let { grid } = this.state;
     let alive_n = 0;
     let dead_n = 0;
 
-    for (let row = i - 1; row <= i + 1; row++) {
-      for (let col = j - 1; col <= j + 1; col++) {
+    for (let row = i - 1; row <= i + 1; row += 1) {
+      for (let col = j - 1; col <= j + 1; col += 1) {
         if (
           // Inside the grid
           row >= 0 &&
@@ -114,7 +110,7 @@ class App extends Component {
           col >= 0 &&
           col < grid.length &&
           // Not count the point we are checking
-          (row !== j || col !== i)
+          (row !== i || col !== j)
         ) {
           // Sum dead and alive around
           grid[row][col] ? alive_n += 1 : dead_n += 1;
@@ -122,12 +118,7 @@ class App extends Component {
       }
     }
 
-    // debugger
-
-    return {
-      alive_n,
-      dead_n
-    }
+    return { alive_n, dead_n }
   }
 
   render() {
