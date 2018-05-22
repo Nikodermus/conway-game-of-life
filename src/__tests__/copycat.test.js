21d5835ff97762ce6bc33b68e728163c59134118
Nicolás M. Pardo <nicolasm@nikodermus.media>
2018-05-22 10:54:18 -0500

    Compare deep arrays + UT

--- src/__tests__/copycat.test.js
+++ src/__tests__/copycat.test.js
@@ -1,4 +1,4 @@
-import { copyArray } from '../helpers/copycat'
+import { copyArray, compareArray } from '../helpers/copycat'
 
 test('Array deep cloning', () => {
   let test_array = [[2], [[1], [2]]];
@@ -6,3 +6,10 @@ test('Array deep cloning', () => {
   test_array[1][0][0] = 'this should not change';
   expect(copy_array).toEqual([[2], [[1], [2]]]);
 });
+
+test('Compare deep arrays', () => {
+  let test_array = [[2], [[1], [2]]];
+  expect(compareArray(test_array, test_array)).toBeTruthy();
+  expect(compareArray(test_array, [[2], [[1], [2]]])).toBeTruthy();
+  expect(compareArray(test_array, [[2], [[2], [2]]])).toBeFalsy();
+})
--- src/helpers/copycat.js
+++ src/helpers/copycat.js
@@ -1,11 +1,26 @@
 export const copyArray = (array) => {
-  let array_copy = []; // An new empty array
+  let array_copy = [];
   for (let index in array) {
+    // If it's not an array, just copy the value
     if (!Array.isArray(array[index])) {
       array_copy[index] = array[index];
     } else {
+      // Recursive copy inner arrays
       array_copy[index] = copyArray(array[index]);
     }
   }
   return array_copy;
+}
+
+export const compareArray = (array_1, array_2) => {
+  // All elements including inner should be the same
+  return array_1.every((elem, index) => {
+    if (Array.isArray(array_1[index])) {
+      // We have an array, so check inner elems
+      return compareArray(array_1[index], array_2[index]);
+    } else {
+      // If not an array, just compare
+      return array_1[index] === array_2[index];
+    }
+  });
 }
