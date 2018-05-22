f4f351e14e5573a3097c6f5b5020bfa59811c5ce
root <root@CO-IT00401.globant.com>
2018-05-22 11:41:53 -0500

    docs(Src): Included App and helper function
    Define docs with jsdoc for App.js, helpers/copycat.js

--- src/helpers/copycat.js
+++ src/helpers/copycat.js
@@ -1,3 +1,7 @@
+/**
+ * Make a deep clone of an array.
+ * @param {array} array - Array to be copied
+ */
 export const copyArray = (array) => {
   let array_copy = [];
   for (let index in array) {
@@ -12,6 +16,13 @@ export const copyArray = (array) => {
   return array_copy;
 }
 
+
+/**
+ * Compare if two arrays that may have nested arrays
+ * are completely equal
+ * @param {array} array_1 - Array to be iterated
+ * @param {array} array_2 - Second array to be compared
+ */
 export const compareArray = (array_1, array_2) => {
   // All elements including inner should be the same
   return array_1.every((elem, index) => {
